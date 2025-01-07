8ebf10cbbff913bda5f3b861c3d4e49501e1145f
shivamsheokand <shivamsheokand32@gmail.com>
2025-01-07 19:25:27 +0530

    user Form

--- app/Http/Controllers/UserForm.php
+++ app/Http/Controllers/UserForm.php
@@ -8,6 +8,13 @@ class UserForm extends Controller
 {
     function addUser(Request $req){
         // echo "user Form submint";
-        return $req;
+        // return $req;
+        echo "User Name is $req->username";
+        echo"<br>";
+        echo "User Email is $req->email";
+        echo"<br>";
+        echo "User City is $req->city";
+        echo"<br>";
+        print_r ( $req->check);
     }
 }
--- resources/views/user-form.blade.php
+++ resources/views/user-form.blade.php
@@ -1,38 +1,121 @@
-<div>
-    <h1> Add User</h1>
+<div class="container">
+    <h1>Add User</h1>
     <form action="addUser" method="post">
         @csrf
-        <div>
-            <h4>Skills</h4>
-            <input type="checkbox" value="PHP" name="skill[]" id="php"/>
-            <label for="php">PHP</label>
-            <input type="checkbox" value="JS" name="skill[]" id="js"/>
-            <label for="js">JS</label>
-            <input type="checkbox" value="Java" name="skill[]" id="java"/>
-            <label for="java">Java</label>
+        <div class="input-wrapper">
+            <input type="text" class="inputf" placeholder="Enter User Name" name="username">
+        </div>
+        <div class="input-wrapper">
+            <input class="inputf" type="email" placeholder="Enter User Email" name="email">
+        </div>
+        <div class="input-wrapper">
+            <input type="text" class="inputf" placeholder="Enter User City" name="city">
         </div>
         <div>
-            <h4>Gender</h4>
-            <input type="radio" value="Male" name="gender" id="male"/>
-            <label for="male">Male</label>
-            <input type="radio" value="Femail" name="gender" id="femail"/>
-            <label for="femail">Femail</label>
-            
+            <fieldset>
+                <legend>Select Your Subjects:</legend>
+                <input type="checkbox" name="check[]" id="checkbox1" value="html" checked>
+                <label for="checkbox1">HTML</label>
+                <br>
+
+                <input type="checkbox" name="check[]" id="checkbox2" value="css">
+                <label for="checkbox2">CSS</label>
+                <br>
+
+                <input type="checkbox" name="check[]" id="checkbox3" value="javascript">
+                <label for="checkbox3">JavaScript</label>
+            </fieldset>
         </div>
         <div>
-            <h4>City</h4>
-            <select name="city">
-                <option value="hisar">Hisar</option>
-                <option value="jind">Jind</option>
-                <option value="Narwana">Narwana</option>
-            </select>
+            <button type="submit">Submit</button>
         </div>
-        <div>
-            <h1>Range</h1>
-            <input type="range" name="age" min="18" max="100">
-        </div>
-    <div>
-        <button>submit</button>
-    </div>
     </form>
 </div>
+
+<style>
+    * {
+        margin: 0;
+        padding: 0;
+        box-sizing: border-box;
+    }
+
+    body {
+        font-family: Arial, sans-serif;
+        background-color: #f4f4f4;
+        color: #333;
+        display: flex;
+        justify-content: center;
+        align-items: center;
+        height: 100vh;
+    }
+
+    .container {
+        background-color: white;
+        border-radius: 8px;
+        box-shadow: 0px 4px 12px rgba(0, 0, 0, 0.1);
+        padding: 20px;
+        width: 400px;
+    }
+
+    h1 {
+        text-align: center;
+        font-size: 24px;
+        margin-bottom: 20px;
+        color: #ff6600;
+    }
+
+    .input-wrapper {
+        margin-bottom: 15px;
+    }
+
+    .inputf {
+        border: 1px solid #ff6600;
+        height: 35px;
+        width: 100%;
+        border-radius: 4px;
+        padding: 5px;
+        color: #ff6600;
+        font-size: 16px;
+        margin-bottom: 10px;
+    }
+
+    input[type="email"] {
+        color: #ff6600;
+    }
+
+    button {
+        border: 1px solid #ff6600;
+        height: 35px;
+        width: 100%;
+        border-radius: 4px;
+        background-color: #ff6600;
+        color: white;
+        font-size: 16px;
+        cursor: pointer;
+        transition: background-color 0.3s ease;
+    }
+
+    button:hover {
+        background-color: #e65c00;
+    }
+
+    fieldset {
+        border: none;
+        margin: 10px 0;
+        padding: 0;
+    }
+
+    legend {
+        font-weight: bold;
+        color: #ff6600;
+    }
+
+    label {
+        margin-left: 5px;
+        font-size: 14px;
+    }
+
+    input[type="checkbox"] {
+        margin-right: 5px;
+    }
+</style>
