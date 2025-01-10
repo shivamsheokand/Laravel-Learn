667ac3115d9b7b17903c71a6cbc898acc5628aef
shivamsheokand <shivamsheokand32@gmail.com>
2025-01-10 21:14:29 +0530

    middleware spacific route

--- routes/web.php
+++ routes/web.php
@@ -1,18 +1,11 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
-
+use  App\Http\Middleware\AgeCheck;
 Route::get('/', function () {
     return view('welcome');
 });
 
-Route::view('about','about')->middleware('check1'); // short way to show view
+Route::view('about','about')->middleware(AgeCheck::class); // short way to show view
 
 
-
-Route::middleware('check1')->group(function(){
-Route::view('home','home');
-Route::view('list','home');
-Route::view('test','home');
-Route::view('cont','home');
-});
