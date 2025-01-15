6c54d8e8ca29f679f08049bd16d07fca16290304
shivamsheokand <shivamsheokand32@gmail.com>
2025-01-15 22:11:19 +0530

    data seeding in laravel

---
+++ database/seeders/TestSeeder.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Str;
+
+class TestSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     */
+    public function run(): void
+    {
+        for($i=1;$i<=1000;$i++){
+            DB::table('tests')->insert([
+            "name"=>Str::random(10),
+            "password"=>Str::random(10),
+            "email"=>Str::random(10).'@gmai.com',
+        ]);
+        }
+    }
+}
